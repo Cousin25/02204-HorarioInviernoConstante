ddd909bd9523c852653058ed9f84b241af767afa
Andrés Cosín Zamarreño <Cousin183@gmail.com>
2019-10-09 21:40:38 +0200

    primer commit

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,32 @@
+/* 
+ * Copyright 2019 Andrés Cosín Zamarreño.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+/**
+ *
+ * @author Andrés Cosín Zamarreño - Cousin183@gmail.com
+ */
+public class Main {
+
+    
+    public static void main(String[] args) {
+        final boolean HORARIO_INVIERNO
+        System.out.print("Suponemos que horario de invierno es cuando anoche antes de las 19 y amanece más tarde de las 8");
+        
+        
+    }
+    
+}
